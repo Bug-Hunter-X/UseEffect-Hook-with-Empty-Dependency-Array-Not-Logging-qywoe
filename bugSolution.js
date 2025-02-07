d79f768acc6e692c541b3c11cb1b200ac168f7e2
BugHunter-X <askzenai@gmail.com>
2025-02-07 15:22:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,24 @@
+```javascript
+import React, { useState, useEffect } from 'react';
+
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log('Mounted!'); // This will now log to the console
+    // Return a cleanup function (optional, but good practice)
+    return () => {
+      console.log('Unmounting!');
+    };
+  }, []);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+}
+
+export default MyComponent;
+```
